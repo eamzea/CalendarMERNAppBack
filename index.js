81f811d36d42ced596c7276870ac737e7c9322ea
Edgar Zea <cesszea@icloud.com>
2021-03-14 19:25:24 -0600

    TEST - Testing Fix error for refresh

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(__dirname + "/public/"));
 app.use(express.json());
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, '/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
